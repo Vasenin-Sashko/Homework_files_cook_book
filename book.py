89536d5c56cad49036922fb2a56992d8193ee059
Vasenin-Sashko <vasenin.as@yandex.ru>
2022-03-29 15:03:59 +0300

    Задача 1. Составлен словарь cook_book

--- book.py
+++ book.py
@@ -1,5 +1,21 @@
-cook_book = {}
+from unicodedata import name
 
-with open('food.txt', 'r', encoding='utf-8') as file:
-    for line in file:
-        print(line.strip())
+
+def create_book(import_file):
+    cook_book = {}
+    
+    with open(import_file, encoding='utf-8') as file:
+        food = [line.strip() for line in file]
+
+        for n, elemant in enumerate(food):
+            if elemant.isdigit():
+                cook_book[food[n-1]] = []
+                for position in food[n+1 : n+int(elemant)+1]:
+                    ingredient_name = position.split('|')[0]
+                    quantity = int(position.split('|')[1])
+                    measure = position.split('|')[2]
+                    cook_book[food[n-1]].append({'ingredient_name':ingredient_name, 'quantity':quantity, 'measure':measure})
+    print(cook_book)
+    return cook_book
+
+create_book('food.txt')
